f4f1ad2b69b3b3dfc9f7ed255597e036497d7f3c
Nguyễn Đức Thành <thanhnd0304@gmail.com>
2018-06-28 16:26:38 +0700

    first commit
    first commit

---
+++ app/src/main/java/ha/thanh/truyenhay/MainActivity.java
@@ -0,0 +1,13 @@
+package ha.thanh.truyenhay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
